8a96bedd9db665f488192434269e1a0bad640466
jesseboon04 <boonjesse04@gmail.com>
2021-01-28 10:29:05 +0100

    Quiz af

---
+++ quiz achievments/quiz1.js
@@ -0,0 +1,73 @@
+const quiz1 =
+{
+    "quizID": "Museum 1",
+    "quizMetaData":
+    {
+        "title": "Quiz over kennis",
+        "imageURI": "https://st.depositphotos.com/1032577/4119/i/450/depositphotos_41197145-stock-photo-quiz.jpg"
+
+    },
+    "quizContent":		
+		[
+			{
+				"question": "In welk land ligt de stad Moskou?",
+				"answers": [{
+						"answer": "Turkije",
+						"feedback": false
+					},
+					{
+						"answer": "Nederland",
+						"feedback": false
+					},
+					{
+						"answer": "Rusland",
+						"feedback": true
+					},
+					{
+						"answer": "Frankrijk",
+						"feedback": false
+					},
+					{
+						"answer": "Japan",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Wat is de hoofdstad van Noord-Holland?",
+				"answers": [{
+						"answer": "Rotterdam",
+						"feedback": false
+					},
+					{
+						"answer": "Den Haag ",
+						"feedback": false
+					},
+					{
+						"answer": "Haarlem ",
+						"feedback": true
+					},
+					{
+						"answer": "Amsterdam",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Wat is het gewicht van 1 kilo water?",
+				"answers": [{
+						"answer": "1000 gram ",
+						"feedback": true
+					},
+					{
+						"answer": "1 kilo",
+						"feedback": true
+					},
+					{
+						"answer": "1000000 miligram",
+						"feedback": true
+					}
+				]
+			}
+		]
+	}
---
+++ quiz achievments/quiz2.js
@@ -0,0 +1,70 @@
+const quiz2 = 
+{
+    "quizID": "Math 1",
+    "quizMetaData":
+    {
+        "title": "Rekenen: gedeelt door en keer",
+        "imageURI": "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/x3y3z3k-1568041112.jpg?crop=0.987xw:0.790xh;0.00160xw,0.128xh&resize=980:*"
+
+    },
+    "quizContent":		
+		[
+			{
+				"question": "wat is: 33 x 3 = ?",
+				"answers": [{
+						"answer": "99",
+						"feedback": true
+					},
+					{
+						"answer": "1",
+						"feedback": false
+					},
+					{
+						"answer": "33",
+						"feedback": false
+					},
+					{
+						"answer": "119",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": " wat is: 650 x 0 =  ?",
+				"answers": [{
+						"answer": "1200",
+						"feedback": false
+					},
+					{
+						"answer": "0",
+						"feedback": true
+					},
+					{
+						"answer": "650",
+						"feedback": false
+					},
+					{
+						"answer": "1300",
+						"feedback": false
+
+					}
+				]
+			},
+			{
+				"question": "Wat is: 1 / 1 = ?",
+				"answers": [{
+						"answer": "1",
+						"feedback": true
+					},
+					{
+						"answer": "0",
+						"feedback": false
+					},
+					{
+						"answer": "2",
+						"feedback": false
+					}
+				]
+			}
+		]
+	}
